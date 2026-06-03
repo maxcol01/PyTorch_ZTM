f08c032cfb0cbc341d5f778f8d119e2bc990674e
Maxime Collet <maxcollet01@gmail.com>
2026-06-03 21:24:39 +0200

    fixing the bug in the visualisitation of the decision boundaries

--- Learning_PyTorch/helper_function.py
+++ Learning_PyTorch/helper_function.py
@@ -52,7 +52,7 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
     if len(torch.unique(y)) > 2:
         y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
     else:
-        y_pred = torch.round(torch.sigmoid(y_logits))
+        y_pred = torch.round(y_logits)
 
     y_pred = y_pred.reshape(xx.shape).detach().numpy()
     plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
